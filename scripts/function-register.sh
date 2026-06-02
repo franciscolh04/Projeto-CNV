12a6972982893a1cdca451c332be100c880fee7e
Eduardo Silva <eduardomartimsilva.09@gmail.com>
2026-06-02 14:58:37 +0100

    fix(function-register): update Lambda function configurations with correct handlers and increased timeout/memory settings

--- scripts/function-register.sh
+++ scripts/function-register.sh
@@ -6,37 +6,37 @@ aws iam create-role \
 	--role-name lambda-role \
 	--assume-role-policy-document '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'
 
-sleep 5
+sleep 8
 
 aws iam attach-role-policy \
 	--role-name lambda-role \
 	--policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
 
-sleep 5
+sleep 8
 
 aws lambda create-function \
 	--function-name fractals-lambda \
 	--zip-file fileb://../fractals/target/fractals-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
-	--handler pt.ulisboa.tecnico.cnv.faas.Handler \
+	--handler pt.ulisboa.tecnico.cnv.fractals.FractalsHandler::handleRequest \
 	--runtime java11 \
-	--timeout 5 \
-	--memory-size 256 \
+	--timeout 60 \
+	--memory-size 1024 \
 	--role arn:aws:iam::$AWS_ACCOUNT_ID:role/lambda-role
 
 aws lambda create-function \
 	--function-name dna-lambda \
 	--zip-file fileb://../dna/target/dna-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
-	--handler pt.ulisboa.tecnico.cnv.faas.Handler \
+	--handler pt.ulisboa.tecnico.cnv.dna.DnaHandler::handleRequest \
 	--runtime java11 \
-	--timeout 5 \
-	--memory-size 256 \
+	--timeout 60 \
+	--memory-size 1024 \
 	--role arn:aws:iam::$AWS_ACCOUNT_ID:role/lambda-role
 
 aws lambda create-function \
 	--function-name grayscott-lambda \
 	--zip-file fileb://../grayscott/target/grayscott-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
-	--handler pt.ulisboa.tecnico.cnv.faas.Handler \
+	--handler pt.ulisboa.tecnico.cnv.grayscott.GrayScottHandler::handleRequest \
 	--runtime java11 \
-	--timeout 5 \
-	--memory-size 256 \
+	--timeout 60 \
+	--memory-size 1024 \
 	--role arn:aws:iam::$AWS_ACCOUNT_ID:role/lambda-role
